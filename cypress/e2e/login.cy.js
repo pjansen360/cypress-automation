5f481f18e5515666642196c5b7e084ae0bcde2c6
unknown <jansen360@live.com>
2026-04-28 09:56:04 -0300

    Estrutura inicial com Cypress

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,14 @@
+describe('Login', () => {
+ it('Realizar Login com Sucesso', () => {
+    //Arrange
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('#user-name').type('standard_user')
+
+    cy.get('#password').type('secret_sauce')
+    //Act
+    cy.get('#login-button').click()
+    //Assert
+    cy.url().should('include', '/inventory')
+    cy.get('.title').should('contain', 'Products')
+ });
+})
